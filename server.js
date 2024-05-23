4080333af9bfe3cd3c8f6b1fc0acf88e01bc59cb
Abdullah <aboood23.ae@gmail.com>
2024-05-23 16:47:19 +0300

    google auth done

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+var session = require('express-session')
+var passport = require('passport')
+require('./config/database')
+require('./config/passport')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
@@ -20,6 +24,18 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(passport.initialize())
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+)
+app.use(function (req, res, next) {
+  res.locals.user = req.user
+  next()
+})
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
