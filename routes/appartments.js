818816c0c1b20c139280f524b750cca5a8ef7305
Abdullah <aboood23.ae@gmail.com>
2024-05-23 14:38:06 +0300

    appartments route done

---
+++ routes/appartments.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+const appartmentCtrl = require('../controllers/appartments')
+
+router.get('/new', appartmentCtrl.new)
+router.post('/', appartmentCtrl.create)
+router.get('/', appartmentCtrl.index)
+router.get('/:id', appartmentCtrl.show)
+
+module.exports = router
