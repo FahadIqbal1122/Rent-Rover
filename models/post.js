18ed2c7f2741e10343c21450401b86242832fa57
fahad <67923371+FahadIqbal1122@users.noreply.github.com>
2024-05-25 22:16:36 +0300

    Delete models/post.js

--- models/post.js
+++
@@ -1,8 +0,0 @@
-// const mongoose = require("mongoose")
-// const PostSchema = new mongoose.Schema({
-//   title_create: {
-//     type: String,
-//     required: true,
-//   },
-// })
-// module.exports = mongoose.model("post", PostSchema)
