99ce8468e2b7383161f9b48e44c4c554a69440a6
Afrah Mohammed <af.moali99@gmail.com>
2024-05-26 14:17:08 +0300

    -

--- routes/appartments.js
+++ routes/appartments.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router()
+
 // Credit for multer = https://www.npmjs.com/package/multer
 const multer = require("multer")
 const appartmentCtrl = require("../controllers/appartments")
@@ -19,15 +20,5 @@ router.get("/new", appartmentCtrl.new)
 
 router.post("/", upload.single("image"), appartmentCtrl.create)
 router.delete("/:id", appartmentCtrl.delete)
-
-router.get("/", appartmentCtrl.index)
-router.get("/:id", appartmentCtrl.show)
-
-// writing as a decoration
-// has a function regex which passes in text argument which is  going to be query
-// function escapeRegex(text) {
-//   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&")
-//   // returns that text and replace with regular expression(match any expression gobally)
-// }
 
 module.exports = router
